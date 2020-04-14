d61c2a13a59f14aff32011cd3826c725d9ae4fa7
azkihidayatulloh <azkihidayatulloh@gmail.com>
2020-04-14 11:15:48 +0700

    update stop condition

--- src/com/azk/Main.java
+++ src/com/azk/Main.java
@@ -40,17 +40,24 @@ public class Main {
 
         // Pelatihan
         // inisialisasi bobot
+        System.out.println("\nBobot awal");
         weights = new double[clusterAmount][inputAmount];
         for (int i = 0; i < weights.length; i++) {
+            System.out.print("Bobot " + (i + 1) + ": ");
             for (int j = 0; j < weights[i].length; j++) {
                 weights[i][j] = Math.random();
+                System.out.print(weights[i][j] + " ");
             }
+            System.out.println();
         }
 
         boolean isStop = false;
         int epoch = 0;
+        double[] deltaMinimum = new double[patternAmount];
         while (!isStop) {
+            System.out.println("Epoch " + (epoch + 1));
             for (int i = 0; i < inputs.length; i++) {
+                System.out.println("Pola " + (i + 1));
                 // untuk setiap pola, hitung delta untuk masing-masing bobot
                 double[] delta = new double[clusterAmount];
                 for (int j = 0; j < delta.length; j++) {
@@ -58,25 +65,37 @@ public class Main {
                     for (int k = 0; k < inputs[i].length; k++) {
                         delta[j] += Math.pow((weights[j][k] - inputs[i][k]), 2);
                     }
+                    System.out.println("D(" + (j + 1) + ") = " + delta[j]);
                 }
 
                 // cari delta terkecil
                 int indexMin = getIndexofSmallestValue(delta);
+                deltaMinimum[i] = delta[indexMin];
 
                 // update bobot dengan delta terkecil
                 for (int j = 0; j < weights[indexMin].length; j++) {
                     double bobotLama = weights[indexMin][j];
                     double bobotBaru = weights[indexMin][j] + alpha * (inputs[i][j] - weights[indexMin][j]);
                     weights[indexMin][j] = bobotBaru;
-                    if (Math.abs(bobotBaru - bobotLama) < r) isStop = true;
+//                    if (Math.abs(bobotBaru - bobotLama) <= r) isStop = true;
                 }
             }
 
             // udpate alpha
             alpha = faktorPenurunan * alpha;
 
+            // uji kondisi berhenti
+            isStop = true;
+            for (double delta : deltaMinimum){
+                if (delta > r) {
+                    isStop = false;
+                    break;
+                }
+            }
+
             epoch++;
             if (epoch >= maxEpoch) isStop = true;
+            System.out.println();
         }
         System.out.println("Pelatihan berhenti di epoch: " + epoch);
 
@@ -87,12 +106,12 @@ public class Main {
             for (int j = 0; j < delta.length; j++) {
                 delta[j] = 0;
                 for (int i = 0; i < input.length; i++) {
-                    delta[j] += Math.pow(( weights[j][i] - input[i]), 2);
+                    delta[j] += Math.pow((weights[j][i] - input[i]), 2);
                 }
             }
 
             int cluster = getIndexofSmallestValue(delta);
-            System.out.println("Cluster pola ke-" + (k+1) + ": " + (cluster + 1));
+            System.out.println("Cluster pola ke-" + (k + 1) + ": " + (cluster + 1));
         }
 
     }
